dfd1ff77f36b0ecdca1f98c9b62bc9290348fb1b
BroJack0809 <79563574+BroJack0809@users.noreply.github.com>
2021-06-30 01:37:04 +0800

    4

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,7 @@ function User(){
             <h3 align="center">學號:B0829006</h3>
             <h3 align="center">興趣:打桌球、羽球、網球</h3>
             <h3 align="center">喜歡的動物:貓</h3>
-            <h2 align="center"><a href="https://jrftxra42kiszlod6h1itq-on.drv.tw/WebFinalB0829006/main.html">個人頁面</a></h2>
+            <h2 align="center"><a href="https://jrftxra42kiszlod6h1itq-on.drv.tw/Myweb/main.html">個人頁面</a></h2>
             <h2 align="center"><a href="https://jrftxra42kiszlod6h1itq-on.drv.tw/WebFinalB0829006/%E7%99%BB%E5%85%A5%E9%A0%81%E9%9D%A2.html">登入頁面</a></h2>
         </div>
     );
--- src/Signin.js
+++ src/Signin.js
@@ -12,7 +12,7 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
